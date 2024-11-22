2efce4183342f4aeb54cae9c3083ef233023eb31
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-23 03:47:38 +0530

    cors handling

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'https://resume-builder-client-auth.vercel.app',
+    credentials: true,
+  }));
+  app.use(express.json());
 
 app.use('/api/resumes', resumeRoutes);
 
